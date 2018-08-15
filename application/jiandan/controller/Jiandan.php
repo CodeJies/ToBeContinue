c458652bd177bc2c1569afabdb7924c846e55083
marki <laungkahung@gmail.com>
2018-08-15 10:16:53 +0800

    imagePathHandle

--- application/jiandan/controller/Jiandan.php
+++ application/jiandan/controller/Jiandan.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace app\jiandan\Controller;
+
 use think\Controller;
 use DOMDocument;
 use DOMXPath;
+
 /**
  * Created by PhpStorm.
  * User: itapp
@@ -12,14 +15,14 @@ use DOMXPath;
 class Jiandan extends Controller
 {
 
-    public function getMeiziList(){
-        $pageIndex=$this->request->post('pageIndex');
-        $data=array();
+    public function getMeiziList()
+    {
+        $pageIndex = $this->request->post('pageIndex');
+        $data = array();
         $header[] = "X-Client-ID:7e43c50781295f355";
         $header[] = "X-Access-Token:4dc049e83308fe6c66ee08a1833577f90298bcec3dca66cc1d20";
-        $tagetUrl='http://jandan.net/ooxx/page-'.$pageIndex.'#comments';
-        $html=doCurlPostRequest($tagetUrl,$data,$header);
-
+        $tagetUrl = 'http://jandan.net/ooxx/page-' . $pageIndex . '#comments';
+        $html = doCurlPostRequest($tagetUrl, $data, $header);
         //创建一个DomDocument对象，用于处理一个HTML
         $dom = new DOMDocument();
         //从一个字符串加载HTML
@@ -32,35 +35,52 @@ class Jiandan extends Controller
         $hrefs = $xpath->evaluate("//*[@id=\"comments\"]/ol/li/div/div/div[2]/p");
 
         for ($i = 0; $i < $hrefs->length; $i++) {
-
             $href = $hrefs->item($i);
             $linktext = $href->nodeValue;
-            $url= base64_decode($linktext);
-            $data= $this->splitData($data,$url);
-
+            $url = base64_decode($linktext);
+            $src = $this->imagePathHander($url);
+            if ($src) $data[] = $src;
+//            $data= $this->splitData($data,$url);
         }
 
-        if($data!=null){
+        if ($data) {
             return jsonString(array_utf8_encode($data));
-        }else{
-            return jsonString(null,"请求失败",0);
+        } else {
+            return jsonString(null, "请求失败", 0);
         }
     }
 
-    public function splitData($data,$str){
-        if(strpos($str,'.gif')){
-            $content=explode('.gif',$str);
-            array_push($data,  str_replace('//','https://',$content[0].'.gif'));
-            $this->splitData($data,base64_decode($content[1]));
-        }else if(strpos($str,'.jpg')){
-            $content=explode('.jpg',$str);
-            array_push($data, str_replace('//','https://',$content[0].'.jpg'));
-            $this->splitData($data,base64_decode($content[1]));
-        }else{
-            array_push($data, str_replace('//','https://',$str));
+    public function splitData($data, $str)
+    {
+        if (strpos($str, '.gif')) {
+            $content = explode('.gif', $str);
+            array_push($data, str_replace('//', 'https://', $content[0] . '.gif'));
+            $this->splitData($data, base64_decode($content[1]));
+        } else if (strpos($str, '.jpg')) {
+            $content = explode('.jpg', $str);
+            array_push($data, str_replace('//', 'https://', $content[0] . '.jpg'));
+            $this->splitData($data, base64_decode($content[1]));
+        } else {
+            array_push($data, str_replace('//', 'https://', $str));
         }
         return $data;
     }
 
+
+    function imagePathHandle($src)
+    {
+        $start = strpos($src, '//ww');
+        $end = strpos($src, '.git');
+        if ($end) {
+            return 'https:' . substr($src, $start, ($end - $start + 4));
+        }
+
+        $end = strpos($src, '.jpg');
+        if ($end) {
+            return 'https:' . substr($src, $start, ($end - $start + 4));
+        }
+
+        return ;
+    }
 
 }
